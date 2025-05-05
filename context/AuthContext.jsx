eb1700cb0944d9eb6c4c1a732c5f637b581357e8
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-05-05 12:28:40 +0100

    auth context file

---
+++ context/AuthContext.jsx
@@ -0,0 +1,88 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
+import {router} from 'expo-router';
+
+const authReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_error':
+            return {...state, errorMessage: action.payload};
+        case 'signin':
+            return {errorMessage: '', token: action.payload};
+        case 'clear_error_message':
+            return {...state, errorMessage: ''};
+        case 'signout':
+            return {token: null, errorMessage: ''};
+        default:
+            return state;
+    }
+};
+
+const tryLocalSignin = (dispatch) => async () => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+        dispatch({type: 'signin', payload: token});
+        router.replace('(tracks)/trackList');
+    } else {
+        router.replace('/signin');
+    }
+};
+
+const clearErrorMessage = (dispatch) => () => {
+    dispatch({type: 'clear_error_message'});
+};
+
+const signup =
+    (dispatch) =>
+    async ({email, password}) => {
+        console.log(email, password);
+        try {
+            const response = await trackerApi.post('/signup', {
+                email,
+                password,
+            });
+            console.log(response.data);
+            await AsyncStorage.setItem('token', response.data.token);
+            dispatch({type: 'signin', payload: response.data.token});
+
+            router.replace('(tracks)/trackList'); // or router.push('/tracklist');
+        } catch (err) {
+            console.log(response.data);
+            dispatch({
+                type: 'add_error',
+                payload: 'Something went wrong with sign up',
+            });
+        }
+    };
+
+const signin =
+    (dispatch) =>
+    async ({email, password}) => {
+        try {
+            const response = await trackerApi.post('/signin', {
+                email,
+                password,
+            });
+            console.log('token data', response.data);
+            await AsyncStorage.setItem('token', response.data.token);
+            dispatch({type: 'signin', payload: response.data.token});
+            router.replace('(tracks)/trackList');
+        } catch (err) {
+            dispatch({
+                type: 'add_error',
+                payload: 'Something went wrong with sign in',
+            });
+        }
+    };
+
+const signout = (dispatch) => async () => {
+    await AsyncStorage.removeItem('token');
+    dispatch({type: 'signout'});
+    router.replace('/');
+};
+
+export const {Provider, Context} = createDataContext(
+    authReducer,
+    {signin, signout, signup, clearErrorMessage, tryLocalSignin},
+    {token: null, errorMessage: ''}
+);
